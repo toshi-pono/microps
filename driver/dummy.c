c5b45913897a03869ca362be515028f36140cf7f
toshi-pono <toshi.pono.h@gmail.com>
2022-03-03 07:24:59 +0000

    dummy.cに大枠を追記

--- driver/dummy.c
+++ driver/dummy.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 
 #include "net.h"
+#include "platform.h"
 #include "util.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int dummy_transmit(struct net_device *dev, uint16_t type,
                           const uint8_t *data, size_t len, const void *dst) {
@@ -14,6 +17,8 @@ static int dummy_transmit(struct net_device *dev, uint16_t type,
   /* drop data */
   return 0;
 }
+
+static int dummy_isr(unsigned int irq, void *id) {}
 
 static struct net_device_ops dummy_ops = {
     .transmit = dummy_transmit,
