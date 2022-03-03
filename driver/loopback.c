51b0837db7883b544c0c56573eccb675589f6b27
toshi-pono <toshi.pono.h@gmail.com>
2022-03-03 08:41:56 +0000

    loopback driverのテンプレートを写経

---
+++ driver/loopback.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "net.h"
+#include "platform.h"
+#include "util.h"
+
+#define LOOPBACK_MTU_UINT16_MAX /* maximum size of IP datagram */
+#define LOOPBACK_QUEUE_LIMIT 16
+#define LOOPBACK_IRQ (INTR_IRQ_BASE + 1)
+
+#define PRIV(x) ((struct loopback *)x->priv)
+
+struct loopback {
+  int irq;
+  mutex_t mutex;
+  struct queue_head queue;
+};
+
+struct loopback_queue_entry {
+  uint16_t type;
+  size_t len;
+  uint8_t data[]; /* flexible array member */
+};
+
+static int loopback_transmit(struct net_device *dev, uint16_t type,
+                             const uint8_t *data, size_t len, const void *dst) {
+
+}
+
+static int loopback_isr(unsigned int irq, void *id) {}
+
+static struct net_device_ops loopback_ops = {
+    .transmit = loopback_transmit,
+};
+
+struct net_device *loopback_init(void) {}
