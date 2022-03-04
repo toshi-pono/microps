419df0a470dc03f527b8a1cc089b629ae392ba05
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-04 10:14:12 +0000

    step10

--- icmp.c
+++ icmp.c
@@ -8,7 +8,7 @@
 
 struct icmp_hdr {
   uint8_t type;
-  uint8_t conde;
+  uint8_t code;
   uint16_t sum;
   uint32_t values;
 };
@@ -49,16 +49,50 @@ static char *icmp_type_ntoa(uint8_t type) {
   return "Unknow";
 }
 
-static void icmp_dump(const uint8_t *data, size_t len) {}
+static void icmp_dump(const uint8_t *data, size_t len) {
+  struct icmp_hdr *hdr;
+  struct icmp_echo *echo;
+
+  flockfile(stderr);
+  hdr = (struct icmp_hdr *)data;
+  fprintf(stderr, "     type: %u (%s)\n", hdr->type, icmp_type_ntoa(hdr->type));
+  fprintf(stderr, "     code: %u\n", hdr->code);
+  fprintf(stderr, "      sum: 0x%04x\n", ntoh16(hdr->sum));
+  switch (hdr->type) {
+    case ICMP_TYPE_ECHOREPLY:
+    case ICMP_TYPE_ECHO:
+      echo = (struct icmp_echo *)hdr;
+      fprintf(stderr, "       id: %u\n", ntoh16(echo->id));
+      fprintf(stderr, "      seq: %u\n", ntoh16(echo->seq));
+      break;
+    default:
+      fprintf(stderr, "   values: 0x%08x\n", ntoh32(hdr->values));
+      break;
+  }
+#ifdef HEXDUMP
+  hexdump(stderr, data, len);
+#endif
+  funlockfile(stderr);
+}
 
 void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst,
                 struct ip_iface *iface) {
+  struct icmp_hdr *hdr;
   char addr1[IP_ADDR_STR_LEN];
   char addr2[IP_ADDR_STR_LEN];
 
+  if (len < ICMP_HDR_SIZE) {
+    errorf("too short");
+    return;
+  }
+  if (cksum16(data, len, 0) != 0) {
+    errorf("checksum diffrent [cksum=0x%04x]", cksum16(data, len, 0));
+    return;
+  }
+
   debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
          ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
-  debugdump(data, len);
+  icmp_dump(data, len);
 }
 
 int icmp_init(void) {
