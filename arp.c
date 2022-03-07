24ed1a14bb761a71e96bce663364911014825e27
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-07 10:11:48 +0000

    step15

--- arp.c
+++ arp.c
@@ -171,6 +171,24 @@ static struct arp_cache *arp_cache_insert(ip_addr_t pa, const uint8_t *ha) {
   return cache;
 }
 
+static int arp_request(struct net_iface *iface, ip_addr_t tpa) {
+  struct arp_ether_ip request;
+
+  request.hdr.hrd = hton16(ARP_HRD_ETHER);
+  request.hdr.pro = hton16(ARP_PRO_IP);
+  request.hdr.hln = ETHER_ADDR_LEN;
+  request.hdr.pln = IP_ADDR_LEN;
+  request.hdr.op = hton16(ARP_OP_REQUEST);
+  memcpy(request.sha, iface->dev->addr, ETHER_ADDR_LEN);
+  memcpy(request.spa, &((struct ip_iface *)iface)->unicast, IP_ADDR_LEN);
+  memcpy(request.tha, ETHER_ADDR_BROADCAST, ETHER_ADDR_LEN);
+  memcpy(request.tpa, &tpa, IP_ADDR_LEN);
+  debugf("dev=%s, len=%zu", iface->dev->name, sizeof(request));
+  arp_dump((uint8_t *)&request, sizeof(request));
+  return net_device_output(iface->dev, ETHER_TYPE_ARP, (uint8_t *)&request,
+                           sizeof(request), iface->dev->broadcast);
+}
+
 static int arp_reply(struct net_iface *iface, const uint8_t *tha, ip_addr_t tpa,
                      const uint8_t *dst) {
   struct arp_ether_ip reply;
@@ -207,8 +225,23 @@ int arp_resolve(struct net_iface *iface, ip_addr_t pa, uint8_t *ha) {
   cache = arp_cache_select(pa);
   if (!cache) {
     debugf("cache not found, pa=%s", ip_addr_ntop(pa, addr1, sizeof(addr1)));
+
+    cache = arp_cache_alloc();
+    if (!cache) {
+      errorf("arp_cache_alloc() failure");
+      return ARP_RESOLVE_ERROR;
+    }
+    cache->state = ARP_CACHE_STATE_INCOMPLETE;
+    cache->pa = pa;
+    gettimeofday(&cache->timestamp, NULL);
     mutex_unlock(&mutex);
-    return ARP_RESOLVE_ERROR;
+    arp_request(iface, pa);
+    return ARP_RESOLVE_INCOMPLETE;
+  }
+  if (cache->state == ARP_CACHE_STATE_INCOMPLETE) {
+    mutex_unlock(&mutex);
+    arp_request(iface, pa); /* just in case packet loss */
+    return ARP_RESOLVE_INCOMPLETE;
   }
   memcpy(ha, cache->ha, ETHER_ADDR_LEN);
   mutex_unlock(&mutex);
