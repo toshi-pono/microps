f52f1b4fb6f94be4435506da59e4d30851f6b106
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-07 08:25:56 +0000

    step14 base

--- arp.c
+++ arp.c
@@ -3,10 +3,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "ether.h"
 #include "ip.h"
 #include "net.h"
+#include "platform.h"
 #include "util.h"
 
 /* see https://www.iana.org/assignments/arp-parameters/arp-parameters.txt */
@@ -16,6 +18,13 @@
 
 #define ARP_OP_REQUEST 1
 #define ARP_OP_REPLY 2
+
+#define ARP_CACHE_SIZE 32
+
+#define ARP_CACHE_STATE_FREE 0
+#define ARP_CACHE_STATE_INCOMPLETE 1
+#define ARP_CACHE_STATE_RESOLVED 2
+#define ARP_CACHE_STATE_STATIC 3
 
 struct arp_hdr {
   uint16_t hrd;
@@ -32,6 +41,16 @@ struct arp_ether_ip {
   uint8_t tha[ETHER_ADDR_LEN];
   uint8_t tpa[IP_ADDR_LEN];
 };
+
+struct arp_cache {
+  unsigned char state;
+  ip_addr_t pa;
+  uint8_t ha[ETHER_ADDR_LEN];
+  struct timeval timestamp;
+};
+
+static mutex_t mutex = MUTEX_INITIALIZER;
+static struct arp_cache caches[ARP_CACHE_SIZE];
 
 static char *arp_opcode_ntoa(uint16_t opcode) {
   switch (ntoh16(opcode)) {
@@ -70,6 +89,21 @@ static void arp_dump(const uint8_t *data, size_t len) {
   funlockfile(stderr);
 }
 
+/*
+ * ARP Cache
+ *
+ * NOTE: ARP Cache functions must be called after mutex locked
+ */
+static void arp_cache_delete(struct arp_cache *cache) {}
+
+static struct arp_cache *arp_cache_alloc(void) {}
+
+static struct arp_cache *arp_cache_select(ip_addr_t pa) {}
+
+static struct arp_cache *arp_cache_update(ip_addr_t pa, const uint8_t *ha) {}
+
+static struct arp_cache *arp_cache_insert(ip_addr_t pa, const uint8_t *ha) {}
+
 static int arp_reply(struct net_iface *iface, const uint8_t *tha, ip_addr_t tpa,
                      const uint8_t *dst) {
   struct arp_ether_ip reply;
@@ -88,6 +122,8 @@ static int arp_reply(struct net_iface *iface, const uint8_t *tha, ip_addr_t tpa,
   return net_device_output(iface->dev, ETHER_TYPE_ARP, (uint8_t *)&reply,
                            sizeof(reply), dst);
 }
+
+int arp_resolve(struct net_iface *iface, ip_addr_t pa, uint8_t *ha) {}
 
 static void arp_input(const uint8_t *data, size_t len, struct net_device *dev) {
   struct arp_ether_ip *msg;
