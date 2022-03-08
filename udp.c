b1ad86d075078a8464c67cbdbb4f714ac04dec42
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-08 05:32:45 +0000

    step18 base

---
+++ udp.c
@@ -0,0 +1,36 @@
+#include "udp.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "ip.h"
+#include "util.h"
+
+struct pseudo_hdr {
+  uint32_t src;
+  uint32_t dst;
+  uint8_t zero;
+  uint8_t protocol;
+  uint16_t len;
+};
+
+struct udp_hdr {
+  uint16_t src;
+  uint16_t dst;
+  uint16_t len;
+  uint16_t sum;
+};
+
+static void udp_dump(const uint8_t *data, size_t len) {}
+
+static void udp_input(const uint8_t *data, size_t len, ip_addr_t src,
+                      ip_addr_t dst, struct ip_iface *iface) {}
+
+ssize_t udp_output(struct ip_endpoint *src, struct ip_endpoint *dst,
+                   const uint8_t *data, size_t len) {}
+
+int udp_init(void) {}
