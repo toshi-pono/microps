3813fdf27f488d88116b3f01b57b717754df6f1b
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-03-08 10:54:06 +0000

    step21 base

---
+++ platform/linux/sched.c
@@ -0,0 +1,16 @@
+#include <errno.h>
+#include <pthread.h>
+#include <time.h>
+
+#include "platform.h"
+
+int sched_ctx_init(struct sched_ctx *ctx) {}
+
+int sched_ctx_destroy(struct sched_ctx *ctx) {}
+
+int sched_sleep(struct sched_ctx *ctx, mutex_t *mutex,
+                const struct timespec *abstime) {}
+
+int sched_wakeup(struct sched_ctx *ctx) {}
+
+int sched_interrupt(struct sched_ctx *ctx) {}
--- udp.c
+++ udp.c
@@ -1,5 +1,6 @@
 #include "udp.h"
 
+#include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -234,6 +235,8 @@ ssize_t udp_output(struct ip_endpoint *src, struct ip_endpoint *dst,
   }
   return len;
 }
+
+static void event_handler(void *arg) {}
 
 int udp_init(void) {
   if (ip_protocol_register(IP_PROTOCOL_UDP, udp_input) == -1) {
